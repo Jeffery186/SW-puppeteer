cb745a3756eaa94ce9bbab8dd9b93368d8199ae7
csd4017 <csd4017@csd.uoc.gr>
2020-08-19 10:55:53 +0300

    for remote change

--- program/first.test.js
+++ program/first.test.js
@@ -5,8 +5,7 @@ describe("Just run the browser", () => {
     it("Should run the browser", async function(){
         //open the browser and a new page
         const browser = await puppeteer.launch({
-            headless: true,
-            slowMo: 0,
+            headless: false,
             defaultViewport: {
                 width: 1500,
                 height: 1000
@@ -18,13 +17,16 @@ describe("Just run the browser", () => {
         const page = pages[0];
 
         //add the puppeteer code
-        context.overridePermissions("https://esmo.pro/play-2_1?h=waWQiOjEwMTYzODgsInNpZCI6MTAyMTc5Niwid2lkIjo4Njc1NCwic3JjIjoyfQ==eyJ&click_id=4e05649cc3263667374d2bd031e6bb1c-9964-0723", ["notifications"])
+        await page.goto("https://www.google.com");
+
+        /*context.overridePermissions("https://esmo.pro/play-2_1?h=waWQiOjEwMTYzODgsInNpZCI6MTAyMTc5Niwid2lkIjo4Njc1NCwic3JjIjoyfQ==eyJ&click_id=4e05649cc3263667374d2bd031e6bb1c-9964-0723", ["notifications"])
         for(i = 0; i < 5; i++){
             await page.setUserAgent(random_useragent.getRandom());
             await page.goto("https://esmo.pro/play-2_1?h=waWQiOjEwMTYzODgsInNpZCI6MTAyMTc5Niwid2lkIjo4Njc1NCwic3JjIjoyfQ==eyJ&click_id=4e05649cc3263667374d2bd031e6bb1c-9964-0723");
             await page.waitFor(5000);
             console.log(await page.url());
-        }
+        }*/
+
         await page.waitFor(3000);
 
         //close the browser
