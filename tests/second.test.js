c02e3f6412b25e35f72cce7bf69d1d57c39cfd07
csd4017 <csd4017@csd.uoc.gr>
2020-08-20 13:43:40 +0300

    the code if a site register a SW

--- tests/second.test.js
+++ tests/second.test.js
@@ -1,47 +1,53 @@
+'use strict'
+
 const puppeteer = require('puppeteer');
 
-describe("Just run the browser", () => {
-    it("Should run the browser", async() => {
-        try {
-            // Step 1: launch browser and open a new page.
-            const browser = await puppeteer.launch({
-                headless: false,
+let url_list = [
+    "https://www.google.com/",
+    "https://www.youtube.com/",
+    "https://github.com/",
+    "https://www.instagram.com/"
+]
+
+
+
+describe("Running for various sites", () => {
+    for (let i = 0; i < url_list.length; i++){
+        it("Checking " + url_list[i], async() => {
+
+            // Step 1: launch browser and take the page.
+            let browser = await puppeteer.launch({
+                headless: true,
                 defaultViewport: {
                     width: 1500,
                     height: 1000
                 },
                 devtools: false
             });
-            const pages = await browser.pages();
-            const page = pages[0];
-            
-            let url_list = [
-                "https://www.google.com/",
-                "https://www.youtube.com/",
-                "https://github.com/",
-                "https://www.instagram.com/"
-            ]
+            let pages =  await browser.pages();
+            let page = pages[0];
 
-            for (let i = 0; i < url_list.length; i++){
+            var swTarget;
+            var url;
+            try {
                 // Step 2: Go to a URL and wait for a service worker to register.
                 url = url_list[i]
                 await page.goto(url)
-                const swTarget = await browser.waitForTarget(target => target.type() === 'service_worker',{
-                    timeout: 5000
-                })
+                swTarget = await browser.waitForTarget(target => target.type() === 'service_worker', {
+                    timeout: 10000
+                });
             
                 // Step 3a: If a service worker is registered, print URL of SW file to the console 
                 if(swTarget) {
-                    console.log(swTarget._targetInfo['url'])
+                    console.log(swTarget._targetInfo['url']);
                 }
+            }catch(err){
+                // The process will timeout after 30s, if no service worker is registered
+                console.error(err.message);
             }
-            
+
             // Step 4: Done. Close.
             await browser.close()
-        
-            } catch (err) {
-            // The process will timeout after 30s, if no service worker is registered
-            console.error(err.message)
-            }
-    })
+        })
+    }
 })
