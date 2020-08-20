a392f6f38ad1fceefb5aa44e58ba7b47d68515c6
csd4017 <csd4017@csd.uoc.gr>
2020-08-20 14:04:04 +0300

    instructions added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SW-puppeteer
+
+All existing tests are on the folder .\tests
+
+For running the tests:
+    1) you need to have node on your computer installed.
+    2) run the command "npm install" on your terminal in this file.
+    3) run the command "npm run test:" + (the name of the test you want to run)
+        e.g. "npm run test:first"
--- tests/second.test.js
+++ tests/second.test.js
@@ -2,18 +2,20 @@
 
 const puppeteer = require('puppeteer');
 
+//list of all the urls we want to check
 let url_list = [
     "https://www.google.com/",
     "https://www.youtube.com/",
     "https://github.com/",
-    "https://www.instagram.com/"
+    "https://www.instagram.com/",
+    "https://esmo.pro/play-2_1?h=waWQiOjEwMTYzODgsInNpZCI6MTAyMTc5Niwid2lkIjo4Njc1NCwic3JjIjoyfQ==eyJ&click_id=4e05649cc3263667374d2bd031e6bb1c-9964-0723"
 ]
 
 
 
-describe("Running for various sites", () => {
+describe("Checking for various sites", () => {
     for (let i = 0; i < url_list.length; i++){
-        it("Checking " + url_list[i], async() => {
+        it("Checked " + url_list[i], async() => {
 
             // Step 1: launch browser and take the page.
             let browser = await puppeteer.launch({
@@ -24,17 +26,20 @@ describe("Running for various sites", () => {
                 },
                 devtools: false
             });
+            let context = browser.defaultBrowserContext();
             let pages =  await browser.pages();
             let page = pages[0];
 
+            let url = url_list[i];
+
             var swTarget;
-            var url;
+
             try {
                 // Step 2: Go to a URL and wait for a service worker to register.
-                url = url_list[i]
+                await context.overridePermissions(url, ["notifications"])
                 await page.goto(url)
                 swTarget = await browser.waitForTarget(target => target.type() === 'service_worker', {
-                    timeout: 10000
+                    timeout: 20000
                 });
             
                 // Step 3a: If a service worker is registered, print URL of SW file to the console 
@@ -42,8 +47,8 @@ describe("Running for various sites", () => {
                     console.log(swTarget._targetInfo['url']);
                 }
             }catch(err){
-                // The process will timeout after 30s, if no service worker is registered
-                console.error(err.message);
+                // The process will timeout after 10s, if no service worker is registered
+                console.log("No SW is registered");
             }
 
             // Step 4: Done. Close.
