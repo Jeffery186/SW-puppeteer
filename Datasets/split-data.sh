d90b652cdec4a5b077bea83ffb8d261c07cdf4be
George Pantelakis <csd4017@csd.uoc.gr>
2020-09-04 14:03:30 +0300

    made bash files

---
+++ Datasets/split-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dif=$(($(cat $1 | wc -l)/$(nproc) + 1))
+
+rm -rf splited
+mkdir splited
+
+for((i = 0; i < $(nproc); i++)); do
+    awk 'NR == '$(($i * $dif + 1))', NR == '$((($i + 1) * $dif))' {print}' $1 > splitedData/part-$i.csv
+done
