60e0398c8e4463a27170b0d41d34a9a033948e4c
George Pantelakis <csd4017@csd.uoc.gr>
2020-09-04 11:01:50 +0300

    conflict

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -10,6 +10,10 @@ let data;
 
 describe("running the crawler", () => {
 
+    process.argv.forEach(function (val, index, array) {
+        console.log(index + ': ' + val);
+    });
+
     rawData = fs.readFileSync(input_file, {encoding: 'ascii'});
     rawDataList = rawData.split('\n');
 
@@ -23,45 +27,47 @@ describe("running the crawler", () => {
     
     for (let i = 0; i < url_list.length; i++){
         it("("+ (i + 1) + "/" + url_list.length + ") Checked " + url_list[i].split('/')[2], async() => {
-
-            // Step 1: launch browser and take the page.
-            let browser = await puppeteer.launch({
-                headless: true,
-                defaultViewport: {
-                    width: 1500,
-                    height: 1000
-                },
-                devtools: false
-            });
-            let context = browser.defaultBrowserContext();
-            let pages =  await browser.pages();
-            let page = pages[0];
-    
-            let url = url_list[i];
-    
-            var swTarget;
-    
-            try {
-                // Step 2: Go to a URL and wait for a service worker to register.
-                await page.goto(url);
-                await context.overridePermissions(url, ["notifications"]);
-    
-                if(await page.url()[4] !== 's') throw err;
-    
-                swTarget = await browser.waitForTarget(target => target.type() === 'service_worker', {
-                    timeout: 15000
+            for(let reloadLoop = 0; reloadLoop < 3; reloadLoop++){
+                // Step 1: launch browser and take the page.
+                let browser = await puppeteer.launch({
+                    headless: true,
+                    defaultViewport: {
+                        width: 1500,
+                        height: 1000,
+                        isMobile: false
+                    },
+                    devtools: false
                 });
-            
-                // Step 3a: If a service worker is registered, print URL of SW file to the console 
-                if(swTarget) {
-                    ServiceWorkers.push(swTarget._targetInfo['url']);
+                let context = browser.defaultBrowserContext();
+                let pages =  await browser.pages();
+                let page = pages[0];
+        
+                let url = url_list[i];
+        
+                var swTarget;
+        
+                try {
+                    // Step 2: Go to a URL and wait for a service worker to register.
+                    await page.goto(url);
+                    await context.overridePermissions(url, ["notifications"]);
+        
+                    if(await page.url()[4] !== 's') throw err;
+        
+                    swTarget = await browser.waitForTarget(target => target.type() === 'service_worker', {
+                        timeout: 15000
+                    });
+                
+                    // Step 3a: If a service worker is registered, print URL of SW file to the console 
+                    if(swTarget) {
+                        ServiceWorkers.push(swTarget._targetInfo['url']);
+                    }
+                }catch(err){
+                    // The process will timeout after 15s, if no service worker is registered
                 }
-            }catch(err){
-                // The process will timeout after 15s, if no service worker is registered
+        
+                // Step 4: Done. Close.
+                await browser.close();
             }
-    
-            // Step 4: Done. Close.
-            await browser.close()
         })
     }
 
