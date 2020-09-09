54e629fc5faaf82564bdb606362c9e9d22577b25
George Pantelakis <csd4017@csd.uoc.gr>
2020-09-09 13:30:48 +0300

    nothing

--- runCrawlers.sh
+++ runCrawlers.sh
@@ -8,5 +8,5 @@ echo $fileName
 ./Datasets/split-data.sh $1
 
 for((i = 0; i < $(nproc); i++)); do
-    screen -dmS crawler-$i bash -c "npm run crawler:$(($i + 1))"
+    screen -dmS crawler-$i bash -c "npm run crawler:$i"
 done
