7f057faf027e6861e4505171145f5595f4295357
George Pantelakis <csd4017@csd.uoc.gr>
2020-09-09 13:18:53 +0300

    problem with npm variables

--- Datasets/split-data.sh
+++ Datasets/split-data.sh
@@ -2,8 +2,8 @@
 
 dif=$(($(cat $1 | wc -l)/$(nproc) + 1))
 
-rm -rf splited
-mkdir splited
+rm -rf splitedData
+mkdir splitedData
 
 for((i = 0; i < $(nproc); i++)); do
     awk 'NR == '$(($i * $dif + 1))', NR == '$((($i + 1) * $dif))' {print}' $1 > splitedData/part-$i.csv
--- runCrawlers.sh
+++ runCrawlers.sh
@@ -7,8 +7,6 @@ echo $fileName
 
 ./Datasets/split-data.sh $1
 
-mkdir results
-
 for((i = 0; i < $(nproc); i++)); do
-    screen -dmS crawler-$i bash -c "mocha --timeout=500000 ./crawler/crawler.js -splitedData/part-$i.csv -$i"
+    screen -dmS crawler-$i bash -c "npm run crawler:$i"
 done
