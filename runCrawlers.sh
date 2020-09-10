99a566edaf8d973b419c7451301bb49b495d46aa
George Pantelakis <csd4017@csd.uoc.gr>
2020-09-10 14:40:46 +0300

    script creator added

--- runCrawlers.sh
+++ runCrawlers.sh
@@ -6,7 +6,7 @@ fileName=${parts[1]}
 echo $fileName
 
 ./Datasets/split-data.sh $1 $2
-python3 updatePackageJson.py
+python3 updatePackageJson.py $2
 
 for((i = 0; i < $2; i++)); do
     screen -dmS crawler-$i bash -c "npm run crawler:$i"
--- updatePackageJson.py
+++ updatePackageJson.py
@@ -1,13 +1,28 @@
 import json
+import sys
+
+numberToStrings = [
+    'first', 'second', 'third', 'forth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth',
+    'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth', 'twentieth'
+    ]
+
+testNumber = 5
 
 with open('package.json', 'r') as fp:
     package = json.load(fp)
 
-for i in range(8):
-    package['script'].pop('crawler:' + i, None)
+for key in package['scripts']:
+    package['scripts'].pop(key, None)
 
-for i in range(16):
-    package['script']['crawler:' + i] = f'mocha --timeout=500000 ./crawler/crawler.js -splitedData/part-{i}.csv -{i}'
+package['scripts']['crawler:test1'] = 'mocha --timeout=500000 ./crawler/crawler.js -Datasets/serviceWorkersSite.csv -1'
+package['scripts']['crawler:test2'] = 'mocha --timeout=500000 ./crawler/crawler.js -Datasets/serviceWorkersSite2.csv -2'
+package['scripts']['crawler'] = 'mocha --timeout=500000 ./crawler/crawler.js -Datasets/top-1m-tranco.csv -1'
+
+for i in range(testNumber):
+    package['scripts']['test:' + numberToStrings[i]] = f'mocha --timeout=500000 ./tests/{numberToStrings[i]}.test.js'
+
+for i in range(int(sys.argv[1])):
+    package['scripts']['crawler:' + str(i)] = f'mocha --timeout=500000 ./crawler/crawler.js -splitedData/part-{i}.csv -{i}'
 
 with open('package.json', 'w') as fp:
     json.dump(package, fp, indent = True)
