b7969f99ab43de59fc3b58655f77e745bdb5f708
csd4017 <csd4017@csd.uoc.gr>
2020-09-21 23:15:51 +0300

    testing about the archive

--- tests/sixth.test.js
+++ tests/sixth.test.js
@@ -1,5 +1,43 @@
 const puppeteer = require("puppeteer");
-const random_useragent = require('random-useragent');
+const scrape = require('website-scraper');
+
+async function downloadSite(site){
+    scrape({
+        urls: [
+            site
+        ],
+        directory: './Downloaded_sites/' + site.split('/')[2],
+        prettifyUrls: true,
+        subdirectories: [
+            {
+                directory: 'img',
+                extensions: ['.jpg', '.png', '.svg', '.webp', '.gif']
+            },
+            {
+                directory: 'js',
+                extensions: ['.js']
+            },
+            {
+                directory: 'css',
+                extensions: ['.css']
+            },
+            {
+                directory: 'php',
+                extensions: ['.php']
+            },
+            {
+                directory: 'fonts',
+                extensions: ['.woff', '.ttf', '.woff2', '.eot']
+            }
+        ]
+    }).then(function (result) {
+        // Outputs HTML 
+        // console.log(result);
+        console.log(site.split('/')[2] + " content succesfully downloaded.");
+    }).catch(function (err) {
+        console.log("Failed to download " + site.split('/')[2] + ".");
+    });
+}
 
 describe("Just run the browser", () => {
     it("Should run the browser", async function(){
@@ -16,10 +54,16 @@ describe("Just run the browser", () => {
         const pages = await browser.pages();
         const page = pages[0];
 
+        page.setDefaultNavigationTimeout(150000);
+
         //add the puppeteer code
-        await page.goto("http://www.google.com", {waitUntil: 'load'});
+        await page.goto("https://www.youtube.com/", {waitUntil: 'load'});
         console.log(await page.url());
-        await page.waitFor(30000);
+        await downloadSite(page.url());
+
+        await page.goto("https://web.archive.org/web/20190514235927/https://www.youtube.com/", {waitUntil: 'load'});
+        console.log(await page.url());
+        await downloadSite(page.url());
 
         //close the browser
         await browser.close();
