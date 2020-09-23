03133a2fafcbf728234251179f1b61567c972c45
csd4017 <csd4017@csd.uoc.gr>
2020-09-24 00:13:58 +0300

    create overAll command

--- createOverAll.sh
+++ createOverAll.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "" > results/ServiceWorkersOverall.txt
+echo "" > results/ServiceWorkers-Overall.txt
 
 for((i = 0; i < $1; i++)); do
-    cat ServiceWorkers-part$i >> results/ServiceWorkersOverall.txt
+    cat ServiceWorkers-part$i >> results/ServiceWorkers-Overall.txt
+done
+
+echo "" > results/noServiceWorkersSites-Overall.txt
+
+for((i = 0; i < $1; i++)); do
+    cat noServiceWorkersSites-part$i >> results/noServiceWorkersSites-Overall.txt
 done
 
 echo "Overall Statistics\n" > Statistics/StatisticsOverall.txt
 echo "=============================\n" >> Statistics/StatisticsOverall.txt
 echo "\n" >> Statistics/StatisticsOverall.txt
-
-OverallSW=0
-OverallSites=0
-
-for((i = 0; i < $1; i++)); do
-    
-done
+echo "$(cat results/ServiceWorkers-Overal | wc -l)/$(($(cat results/ServiceWorkers-Overall.txt | wc -l) + $(cat results/noServiceWorkersSites-Overall.txt | wc -l)))"
