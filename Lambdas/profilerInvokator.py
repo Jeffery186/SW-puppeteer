fb137dc1e85c188c89dacbf3ed2150a82f6dc84c
Panagiotis Papadopoulos <panpap@gmx.com>
2021-04-06 17:14:51 +0100

    edits

--- Lambdas/profilerInvokator.py
+++ Lambdas/profilerInvokator.py
@@ -9,10 +9,26 @@ import json
 import time
 
 LAMBDA_FUNC = "similarweb-crawler"
-#fileList = "sites.txt"
-fileList = "setis.txt"
 
-client = boto3.client('lambda')#, region_name="ap-southeast-2") 
+region=["ap-southeast-2","sa-east-1","af-south-1","us-east-2","us-west-1","us-west-2","ap-northeast-1",
+"ap-southeast-1","ap-northeast-2","ap-northeast-3","eu-north-1","eu-west-2","eu-west-3","eu-west-1",
+"eu-central-1","ca-central-1","ap-south-1"]
+
+if len(sys.argv)<2:
+	print("No input args")
+	sys.exit()
+op = sys.argv[1]
+if (int(op)==16):
+	fileList = "tranco_first_150K"
+elif (int(op)==17):
+	op = 0
+	fileList = "150K_first_tranco"
+else:
+	fileList = "site"+op+".txt"
+
+print("Mode: ",op,"region",region[int(op)],"file",fileList)
+
+client = boto3.client('lambda', region_name=region[int(op)]) 
 with open(fileList) as f:
 	data = f.readlines()
 	for site in data:
